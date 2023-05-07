2f2ee45db9fa362f43cd67ea65b7e5f9639ddc04
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-05-08 07:49:43 +0900

    add admob

--- app/src/main/java/com/b3lon9/easyflash/MainActivity.kt
+++ app/src/main/java/com/b3lon9/easyflash/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.databinding.DataBindingUtil
 import com.b3lon9.easyflash.databinding.ActivityMainBinding
 import com.b3lon9.easyflash.viewmodels.MainViewModel
 import com.b3lon9.nlog.NLog
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         binding.vm = MainViewModel(this, getPreferences(Context.MODE_PRIVATE))    //ViewModelProvider(this, ViewModelFactory(this))[(MainViewModel::class.java)]
+
+        admob()
     }
 
     override fun onResume() {
@@ -27,5 +31,12 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         binding.vm?.pause()
+    }
+
+    private fun admob() {
+        MobileAds.initialize(this)
+
+        val adRequest = AdRequest.Builder().build()
+        binding.banner.loadAd(adRequest)
     }
 }
