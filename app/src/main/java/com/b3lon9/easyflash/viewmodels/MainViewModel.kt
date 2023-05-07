70ad599620ca8c7c5778cf513d4736908dc4bb22
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-05-07 12:07:18 +0900

    add SharedPreference, know ripple effect
    remember switch data

--- app/src/main/java/com/b3lon9/easyflash/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/b3lon9/easyflash/viewmodels/MainViewModel.kt
@@ -4,39 +4,53 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import android.hardware.camera2.CameraManager
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.lifecycle.LiveData
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.b3lon9.easyflash.R
 
-class MainViewModel(private val context: Context, private val pref: SharedPreferences) : ViewModel() {
-    private lateinit var cameraManager:CameraManager
-    private val editor:Editor = pref.edit()
-    val isToggleChecked = MutableLiveData(false)
-    val isSwitchImmediate = MutableLiveData(false)
+class MainViewModel(private val context: Context, private val pref: SharedPreferences) :  ViewModel() {
+    private val cameraManager: CameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    private val editor: Editor = pref.edit()
+
+    val isToggleChecked      = MutableLiveData(false)
+    val isSwitchImmediate    = MutableLiveData(false)
+    val isSwitchScreen       = MutableLiveData(false)
+    val isSwitchLock         = MutableLiveData(false)
 
     init {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        }
         isSwitchImmediate.value = pref.getBoolean(context.getString(R.string.switch_immediate), false)
-
-        if (isSwitchImmediate.value == true) {
-
-        }
+        isSwitchScreen.value = pref.getBoolean(context.getString(R.string.switch_screen), false)
+        isSwitchLock.value = pref.getBoolean(context.getString(R.string.switch_lock), false)
     }
 
-    fun onCheckedChanged(isChecked:Boolean) {
+    fun onCheckedChanged(isChecked: Boolean) {
         isToggleChecked.value = isChecked
         try {
             if (isChecked) flashOn() else flashOff()
         } finally {
-            editor.apply {
-                putBoolean(context.resources.getString(R.string.switch_immediate), isChecked)
-                apply()
+
+        }
+    }
+
+    fun onSwitchToggleChanged(view:View, isChecked: Boolean) {
+        lateinit var key:String
+
+        when(view.id) {
+            R.id.switch_immediate -> {
+                key = context.resources.getString(R.string.switch_immediate)
             }
+            R.id.switch_screen -> {
+                key = context.resources.getString(R.string.switch_screen)
+            }
+            R.id.switch_lock -> {
+                key = context.resources.getString(R.string.switch_lock)
+            }
+        }
+
+        editor.apply {
+            putBoolean(key!!, isChecked)
+            apply()
         }
     }
 
