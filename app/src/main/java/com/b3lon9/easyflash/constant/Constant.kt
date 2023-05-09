2c8a8fa3a766acb26352a4065f824fe04246ff25
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-05-09 19:18:10 +0900

    "1.0.2"
    chage img

---
+++ app/src/main/java/com/b3lon9/easyflash/constant/Constant.kt
@@ -0,0 +1,13 @@
+package com.b3lon9.easyflash.constant
+
+class Constant {
+    object Level {
+        private const val FLASH_LEVEL1 = 1
+        const val FLASH_LEVEL2 = 2
+        const val FLASH_LEVEL3 = 3
+        const val FLASH_LEVEL4 = 4
+        const val FLASH_LEVEL5 = 5
+        const val FLASH_MIN = FLASH_LEVEL1
+        const val FLASH_MAX = FLASH_LEVEL5
+    }
+}
