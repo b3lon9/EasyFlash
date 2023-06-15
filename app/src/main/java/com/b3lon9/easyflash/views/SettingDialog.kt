e6a750add1927e0695f174230faac85834454830
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-06-15 23:43:42 +0900

    modify add setting dialog

--- app/src/main/java/com/b3lon9/easyflash/views/SettingDialog.kt
+++ app/src/main/java/com/b3lon9/easyflash/views/SettingDialog.kt
@@ -20,6 +20,9 @@ class SettingDialog(private val context:Context, private val vm:MainViewModel) :
     private lateinit var binding:SettingDialogBinding
     private lateinit var listener:SettingDataListener
 
+    private var themeColor:Theme = Theme.values()[vm.themeColor]
+    private var screenColor:Screen = Screen.values()[vm.screenColor]
+
     interface SettingDataListener {
         fun onThemeColor(themeColor:Theme)
         fun onScreenColor(screenColor:Screen)
@@ -66,10 +69,6 @@ class SettingDialog(private val context:Context, private val vm:MainViewModel) :
         this.listener = listener
     }
 
-    lateinit var themeColor:Theme
-    lateinit var screenColor:Screen
-
-
     fun onThemeCheckedChanged(@IdRes radioButtonId:Int) {
         this.themeColor = when(radioButtonId) {
             R.id.theme_radio_beige -> Theme.BEIGE
@@ -79,11 +78,17 @@ class SettingDialog(private val context:Context, private val vm:MainViewModel) :
         }
     }
 
+    fun onScreenCheckedChanged(@IdRes radioButtonId:Int) {
+        this.screenColor = when(radioButtonId) {
+            R.id.screen_radio_white_yellow -> Screen.WHITEYELLOW
+            R.id.screen_radio_yellow -> Screen.YELLOW
+            else -> Screen.WHITE
+        }
+    }
+
     override fun onDismiss(dialog: DialogInterface?) {
         NLog.d("onDismiss... themeColor : $themeColor")
         listener.onThemeColor(themeColor)
-        /*if (vm.themeColor.value != themeColor) {
-            listener.onThemeColor(themeColor)
-        }*/
+        listener.onScreenColor(screenColor)
     }
 }
